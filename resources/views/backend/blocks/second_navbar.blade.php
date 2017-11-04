2e69662fc28e6ed83705a900b4ce9a29ad896f8b
Vũ Quốc Tuấn <contact.quoctuan@gmail.com>
2017-11-05 01:52:54 +0700

    update navbar

--- resources/views/backend/blocks/second_navbar.blade.php
+++ resources/views/backend/blocks/second_navbar.blade.php
@@ -8,7 +8,7 @@
 			<li><a href="{{ route('admin.dashboard.index') }}"><i class="icon-display4 position-left"></i> Dashboard</a></li>
 			
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-folder position-left"></i> Category <span class="caret"></span>
 				</a>
 
@@ -20,21 +20,21 @@
 			</li>
 			
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-man-woman position-left"></i> Member<span class="caret"></span>
 				</a>
 
 				<ul class="dropdown-menu width-250">
 					<li class="dropdown-header">Manage Member</li>						
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-users"></i> User</a>
+						<a><i class="icon-users"></i> User</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.user') }}">List User</a></li>
 							<li><a href="{{ route('admin.user.create') }}">Add User</a></li>
 						</ul>
 					</li>
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-key"></i> Role</a>
+						<a><i class="icon-key"></i> Role</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.role') }}">List Role</a></li>
 							<li><a href="{{ route('admin.role.create') }}">Add Role</a></li>
@@ -44,27 +44,27 @@
 			</li>
 
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-stack-text position-left"></i> Article<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu width-250">
 					<li class="dropdown-header">Manage Post</li>						
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-magazine"></i> Post</a>
+						<a><i class="icon-magazine"></i> Post</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.post') }}">List Post</a></li>
 							<li><a href="{{ route('admin.post.create') }}">Add Post</a></li>
 						</ul>
 					</li>
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-price-tags"></i> Tags</a>
+						<a><i class="icon-price-tags"></i> Tags</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.tags') }}">List Tags</a></li>
 							<li><a href="{{ route('admin.tags.create') }}">Add Tags</a></li>
 						</ul>
 					</li>
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-newspaper"></i> News</a>
+						<a><i class="icon-newspaper"></i> News</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.news') }}">List News</a></li>
 							<li><a href="{{ route('admin.news.create') }}">Add News</a></li>
@@ -74,27 +74,27 @@
 			</li>
 
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-grid6 position-left"></i> Product<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu width-250">
 					<li class="dropdown-header">Manage Product</li>						
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-lab"></i> Attribute</a>
+						<a><i class="icon-lab"></i> Attribute</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.attribute') }}">List Attribute</a></li>
 							<li><a href="{{ route('admin.attribute.create') }}">Add Attribute</a></li>
 						</ul>
 					</li>
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-office"></i> Manufacturer</a>
+						<a><i class="icon-office"></i> Manufacturer</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.manufacturer') }}">List Manufacturer</a></li>
 							<li><a href="{{ route('admin.manufacturer.create') }}">Add Manufacturer</a></li>
 						</ul>
 					</li>
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-cube3"></i> Product</a>
+						<a><i class="icon-cube3"></i> Product</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.product') }}">List Product</a></li>
 							<li><a href="{{ route('admin.product.create') }}">Add Product</a></li>
@@ -104,20 +104,20 @@
 			</li>
 
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-images2 position-left"></i> Media<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu width-250">
 					<li class="dropdown-header">Manage Media</li>						
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-move-alt1"></i> Position Banner, Album</a>
+						<a><i class="icon-move-alt1"></i> Position Banner, Album</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.position') }}">List Position</a></li>
 							<li><a href="{{ route('admin.position.create') }}">Add Position</a></li>
 						</ul>
 					</li>
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-image2"></i> Banner , Album</a>
+						<a><i class="icon-image2"></i> Banner , Album</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.banner') }}">List Banner</a></li>
 							<li><a href="{{ route('admin.banner.create') }}">Add Banner</a></li>
@@ -127,7 +127,7 @@
 			</li>
 
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-address-book position-left"></i> Contact <span class="caret"></span>
 				</a>
 
@@ -139,20 +139,20 @@
 			</li>
 
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-mailbox position-left"></i> Mail & Comment<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu width-250">
 					<li class="dropdown-header">Manage Mail & Message</li>						
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-envelop5"></i> Mail</a>
+						<a><i class="icon-envelop5"></i> Mail</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.mail') }}">List Mail</a></li>
 							<li><a href="{{ route('admin.mail.getSend') }}">Sent Mail</a></li>
 						</ul>
 					</li>
 					<li class="dropdown-submenu">
-						<a href="#"><i class="icon-comments"></i> Comment</a>
+						<a><i class="icon-comments"></i> Comment</a>
 						<ul class="dropdown-menu width-200">
 							<li><a href="{{ route('admin.comment') }}">List Comment</a></li>
 						</ul>
@@ -163,7 +163,7 @@
 
 		<ul class="nav navbar-nav navbar-right">
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown">
 					<i class="icon-cog3"></i>
 					<span class="visible-xs-inline-block position-right">Share</span>
 					<span class="caret"></span>
@@ -171,8 +171,8 @@
 
 				<ul class="dropdown-menu dropdown-menu-right">
 					<li><a href="{{ route('admin.user.get-edit-myself') }}"><i class="icon-user-lock"></i> Edit My Account</a></li>
-					<li><a href="#"><i class="icon-statistics"></i> Analytics (Updated)</a></li>
-					<li><a href="#"><i class="icon-accessibility"></i> Accessibility (Updated)</a></li>
+					<li><a><i class="icon-statistics"></i> Analytics (Updated)</a></li>
+					<li><a><i class="icon-accessibility"></i> Accessibility (Updated)</a></li>
 					<li class="divider"></li>
 					<li><a href="{{ route('admin.dashboard.config') }}"><i class="icon-gear"></i> All settings</a></li>
 					<li><a href="{{ route('admin.log.list_action') }}"><i class="icon-gear"></i> Log Action User</a></li>
